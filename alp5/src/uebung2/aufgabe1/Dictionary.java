7f4e12227768978b00d8fe215ecd31a0650d793a
gitmo <seb0rz+github@gmail.com>
2010-11-15 17:53:59 +0100

    Dictionary loads strings in memory on construction.

--- alp5/src/uebung2/aufgabe1/Dictionary.java
+++ alp5/src/uebung2/aufgabe1/Dictionary.java
@@ -3,21 +3,29 @@ package uebung2.aufgabe1;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.util.TreeSet;
+import java.util.Set;
 
 class Dictionary {
 	String dictionaryFile = "english.txt";
-	
-	public boolean contains(String var) throws IOException {
-		String path = this.getClass().getResource(dictionaryFile).getPath();
-		BufferedReader dictionaryReader = new BufferedReader(new FileReader(path));
-		
-		String line = null;
-		
-		while((line = dictionaryReader.readLine()) != null) {
-			if(line.toLowerCase().equals(var))//equals(var))
-				return true;
+	Set<String> dictionary = new TreeSet<String>();
+
+	public Dictionary() throws IOException {
+		BufferedReader dictionaryReader = null;
+		try {
+			String path = this.getClass().getResource(dictionaryFile).getPath();
+			dictionaryReader = new BufferedReader(new FileReader(path));
+		} catch (Exception e) {
+			System.err.printf("ERROR: Dictionary file '%s' not found.\n",
+					dictionaryFile);
 		}
-		return false;
+
+		String line = null;
+		while ((line = dictionaryReader.readLine()) != null)
+			this.dictionary.add(line.toLowerCase());
+	}
+
+	public boolean contains(String s) throws IOException {
+		return dictionary.contains(s);
 	}
 }
