47b42d312b0c07b9ebfa0b68f2f28707035305db
gitmo <seb0rz+github@gmail.com>
2010-11-15 19:18:04 +0100

    Check for english words with same spelling in german.

--- alp5/src/uebung2/aufgabe1/Dictionary.java
+++ alp5/src/uebung2/aufgabe1/Dictionary.java
@@ -7,25 +7,36 @@ import java.util.TreeSet;
 import java.util.Set;
 
 class Dictionary {
-	String dictionaryFile = "english.txt";
-	Set<String> dictionary = new TreeSet<String>();
+	String dictionaryEnFile = "english.txt";
+	String dictionaryDeFile = "german.txt";
+	Set<String> dictionaryEn = new TreeSet<String>();
+	Set<String> dictionaryDe = new TreeSet<String>();
 
 	public Dictionary() throws IOException {
+		readInMemory(dictionaryEnFile, dictionaryEn);
+		readInMemory(dictionaryDeFile, dictionaryDe);
+	}
+
+	private void readInMemory(String file, Set<String> dict) throws IOException {
 		BufferedReader dictionaryReader = null;
 		try {
-			String path = this.getClass().getResource(dictionaryFile).getPath();
+			String path = this.getClass().getResource(file).getPath();
 			dictionaryReader = new BufferedReader(new FileReader(path));
 		} catch (Exception e) {
-			System.err.printf("ERROR: Dictionary file '%s' not found.\n",
-					dictionaryFile);
+			System.err.printf("ERROR: Dictionary file '%s' not found.\n", file);
 		}
 
 		String line = null;
 		while ((line = dictionaryReader.readLine()) != null)
-			this.dictionary.add(line.toLowerCase());
+			dict.add(line.toLowerCase());
 	}
 
+	/**
+	 * Word is in the English dictionary but has not the same spelling as a
+	 * German word.
+	 */
 	public boolean contains(String s) throws IOException {
-		return dictionary.contains(s);
+		String lower = s.toLowerCase();
+		return dictionaryEn.contains(lower) && !dictionaryDe.contains(lower);
 	}
 }
