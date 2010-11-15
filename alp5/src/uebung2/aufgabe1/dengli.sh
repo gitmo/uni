8ed261f19e697b4bc8a24e3a0d3ea749739990ff
gitmo <seb0rz+github@gmail.com>
2010-11-15 16:23:13 +0100

    Demo shell script will run both exercises

--- alp5/src/uebung2/aufgabe1/dengli.sh
+++ alp5/src/uebung2/aufgabe1/dengli.sh
@@ -1,23 +1,38 @@
 #!/bin/sh
-# usage: dengli.sh file1 file2    (needs execute permission!)
-
-set -x
-
-# Copies the resource file (english.txt, etc.) to bin/
-ant clean init 
-ant build
-
+# Asuming we're in src/uebung2/aufgabe1
 export CLASSPATH=../../../bin
 package=uebung2.aufgabe1
 
-echo "Aufgabe 1a)"
+# Change to the same directory this script is in
+cd `dirname $0`
+
+# Check for arguments
+if [ $# -ne 2 ]; then
+  echo "Usage: `basename $0` file1 file2"
+  exit 2
+fi
+
+#set -x         # show trace of every line
+set -o errexit  # exit immediately on non-zero exit status
+
+# During the 'ant init' task (which build depends on) the resource
+# files (english.txt, etc.) are copied to bin/. The build-task then 
+# compiles all sources to bin/ as well.
+ant build
+
+echo
+echo "Exercise 1a)"
+echo "========+==="
 mkfifo one two    
 java $package.Filter "$1" one &
 java $package.Filter "$2" two &
 java $package.Merge one two
 rm one two
 
-echo "Aufgabe 1b)"
-java $package.Denglish localhost localhost test.txt test2.txt
+echo
+echo "Exercise 1b)"
+echo "========+==="
+java $package.Denglish localhost localhost "$1" "$2"
 
-# or: ant Denglish -Dargs='localhost localhost test.txt test2.txt'
+# There's also an ant target instead: 
+#ant Denglish -Dargs="localhost localhost \"$1\" \"$2\""
