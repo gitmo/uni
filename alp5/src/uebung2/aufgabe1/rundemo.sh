bb9abfdb1f0c79438ee216221800ff87d979c1f3
gitmo <seb0rz+github@gmail.com>
2010-11-16 12:38:33 +0100

    Bash script consolidation

--- alp5/src/uebung2/aufgabe1/rundemo.sh
+++ alp5/src/uebung2/aufgabe1/rundemo.sh
@@ -1,7 +1,47 @@
 #!/bin/sh
 
-SSHUSER=$USER
+exercise1a() {
+  mkfifo one two    
+  java $package.Filter "$1" one &
+  java $package.Filter "$2" two &
+  java $package.Merge one two
+  rm one two
+}
 
+exercise1b() {
+  java $package.Denglish "$@"
+  # There's also an ant target 
+  #ant Denglish -Dargs="$@"
+}
+
+demo(){
+  echo
+  echo "Exercise 1a)"
+  echo "============"
+  exercise1a test1.txt test2.txt
+ 
+  echo
+  echo "Exercise 1b)"
+  echo "============"
+  echo 'IMPORTANT: If there are any "Class not found"'
+  echo '           problems adapt0 CLASSPATH in remote.sh!'
+  echo
+  exercise1b $1@andorra.imp.fu-berlin.de $1@nawab.imp.fu-berlin.de \
+             test1.txt test2.txt
+}
+
+
+# Change to the same directory this script is in
+cd `dirname $0`
+
+# Asuming we're in src/uebung2/aufgabe1
+export CLASSPATH=./../../../bin
+package=uebung2.aufgabe1
+
+# During the 'ant init' task (which build depends on) the resource
+# files (english.txt, etc.) are copied to bin/. The build-task then 
+# compiles all sources to bin/ as well.
 ant clean build
-sh exercise1a.sh test1.txt test2.txt
-sh exercise1b.sh $SSHUSER@andorra.imp.fu-berlin.de $SSHUSER@shanghai.imp.fu-berlin.de test1.txt test2.txt
+
+demo $USER
+
