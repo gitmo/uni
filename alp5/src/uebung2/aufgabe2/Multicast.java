8060105bdb1591a3bb4b65576c5f9f574f2893d3
gitmo <seb0rz+github@gmail.com>
2010-11-16 01:35:17 +0100

    Multicast: comments, some refactoring, quit command

--- alp5/src/uebung2/aufgabe2/Multicast.java
+++ alp5/src/uebung2/aufgabe2/Multicast.java
@@ -3,27 +3,37 @@ package uebung2.aufgabe2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
-import java.net.Socket;
-import java.net.UnknownHostException;
 
 /**
- * Multicast Chat-Client
+ * Multicast chat system
  * 
  * @author xxx & xxx (Gruppe xxx)
  */
 public class Multicast {
-	private static final String PROMPT = "> ";
+	// Multicast: 224.0.0.1 - 239.255.255.255
+	private static final String CHATGROUP = "229.1.2.3";
 	private static final int PORT = 56789;
-	InetAddress group;
-	MulticastSocket socket;
-	String name;
 
-	public static void main(String[] args) throws UnknownHostException,
-			IOException, InterruptedException {
+	// Chat prompt and command to leave chat.
+	private static final String PROMPT = "> ";
+	private static final String QUIT = ":q"; // Enter to leave chat
+
+	// Private fields used in between threads.
+	private InetAddress group;
+	private MulticastSocket socket;
+	private String name;
+
+	/**
+	 * Run a Multicast chat session.
+	 * 
+	 * @param args
+	 *            Chat client's name.
+	 */
+	public static void main(String[] args) throws IOException,
+			InterruptedException {
 
 		if (args.length < 1) {
 			System.err.println("Usage: java Multicast <name>");
@@ -32,28 +42,39 @@ public class Multicast {
 		new Multicast(args[0]);
 	}
 
+	/**
+	 * Control flow of chat system. Setup and tear down of Multicast
+	 * communication.
+	 */
 	public Multicast(String name) throws IOException, InterruptedException {
 		// Set client name
 		this.name = name;
 		// join a Multicast group
-		// 224.0.0.1 - 239.255.255.255
-		group = InetAddress.getByName("229.1.2.3");
+		group = InetAddress.getByName(CHATGROUP);
 		socket = new MulticastSocket(PORT);
 		socket.joinGroup(group);
+		// Start receiving and sending threads
+		Thread tr, ts;
+		(tr = new Thread(receiver)).start();
+		(ts = new Thread(sender)).start();
 
-		Thread tr = new Thread(receiver);
-		Thread ts = new Thread(sender);
-
-		tr.start();
-		ts.start();
-
+		// Wait for sender to quit.
+		System.out.println("Start chatting. Ctrl-D or " + QUIT + " to quit.");
 		ts.join();
-		tr.join();
 
 		// OK, I'm done talking - leave the group...
+		System.out.println("Leaving group.");
 		socket.leaveGroup(group);
+		// Hangup
+		socket.close();
+		// Waiting for receiver to give up.
+		tr.join();
+		System.out.println("Done.");
 	}
 
+	/**
+	 * Sending thread. Sends every line from console until Ctrl-D or ':q'.
+	 */
 	Runnable sender = new Runnable() {
 		DatagramPacket send;
 		BufferedReader input = new BufferedReader(new InputStreamReader(
@@ -65,8 +86,8 @@ public class Multicast {
 			try {
 				while (true) {
 					String readLine = input.readLine();
-					// EOF (^D) received
-					if (readLine == null)
+					// EOF (^D) or ':q' received
+					if (readLine == null || readLine.equalsIgnoreCase(QUIT))
 						break;
 
 					String msg = name + ": " + readLine;
@@ -80,6 +101,9 @@ public class Multicast {
 		}
 	};
 
+	/**
+	 * Receiving thread. Quits on error or socket closed.
+	 */
 	Runnable receiver = new Runnable() {
 		byte[] buf = new byte[1024];
 		DatagramPacket recv = new DatagramPacket(buf, buf.length);
@@ -97,7 +121,7 @@ public class Multicast {
 					System.out.print(PROMPT);
 				}
 			} catch (IOException e) {
-				e.printStackTrace();
+				System.out.println("Receiver quit.");
 			}
 		}
 	};
