d719a0c5d93cd5085e3f905bcd8ea6818fa93676
gitmo <seb0rz+github@gmail.com>
2010-11-30 11:56:55 +0100

    synchronizedSortedSet...

--- alp5/src/uebung3/aufgabe1b/RemoteSet.java
+++ alp5/src/uebung3/aufgabe1b/RemoteSet.java
@@ -2,12 +2,14 @@ package uebung3.aufgabe1b;
 
 import java.rmi.RemoteException;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Set;
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class RemoteSet<T> implements IRemoteSet<T> {
 	private static final long serialVersionUID = 2601212913645664134L;
-	private TreeSet<T> set = new TreeSet<T>();
+	private SortedSet<T> set = Collections.synchronizedSortedSet(new TreeSet<T>());
 
 	public boolean remoteAddAll(Collection<T> collection) {
 		return set.addAll(collection);
