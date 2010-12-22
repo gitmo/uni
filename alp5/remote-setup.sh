bdb96b16569eeed41c4c3affd0bf54b0cd1465b3
gitmo <seb0rz+github@gmail.com>
2010-12-22 10:59:17 +0100

    Script remote-setup.sh to quickly transfer and build project on uni servers

---
+++ alp5/remote-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Change to the same directory this script is in
+cd `dirname $0`
+
+USER=xxx
+FU=$USER@lounge
+REMDIR=git/uni/alp5
+
+rsync -av $(pwd)/src $FU:$REMDIR && \
+  ssh -q -o "BatchMode yes" $FU "cd $REMDIR; ant"
+
+for h in nawab lounge andorra; do
+  ssh -f -o "BatchMode yes" $USER@$h 'killall -u $USER java' >/dev/null 2>&1 &
+done
+
