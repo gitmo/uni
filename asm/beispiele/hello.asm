ce2bc025cdfd09af9d7896d41bbcf1b47ead554c
gitmo <seb0rz+github@gmail.com>
2011-02-18 00:18:01 +0100

    Hello World

--- asm/beispiele/hello.asm
+++ asm/beispiele/hello.asm
@@ -1,26 +1,63 @@
+; Hello World using nasm on OS X
 ;
-;
-;
+; nasm -f macho hello.asm && ld hello.o -o hello && ./hello
 
-%include	'system.inc'
+; preprocessor macros
+%define stdin   0
+%define stdout  1
+%define stderr  2
 
-; static data
-section	.data
+%define SYS_nosys   0
+%define SYS_exit    1
+%define SYS_fork    2
+%define SYS_read    3
+%define SYS_write   4
 
-hello	db  'Hello, World!', 0Ah
-hbytes	equ	$ - hello
+; instruct nasm to assemble for 32-bit
+BITS 32
 
-; code
-section	.text
+; code segment
+section .text
 
-global	start
+global  start                   ; make the main function externally visible
+
+; By default, the FreeBSD kernel uses the C calling convention. Further, although
+; the kernel is accessed using int 80h, it is assumed the program will call a
+; function that issues int 80h, rather than issuing int 80h directly.
+; -- http://www.int80h.org/bsdasm/
+kernel:
+    int     80h                 ; Call kernel
+    ret
 
 start:
-        push	dword hbytes
-        push	dword hello
-        push	dword stdout
-        sys.write
+    ; 1 print "hello, world"
 
-        push	dword 0
-        sys.exit
+    ; 1a prepare the arguments for the system call to write
+    push    dword length        ; length
+    push    dword message       ; string
+    push    dword stdout        ; file descriptor value
+
+    ; 1b make the system call to write
+    mov     ax, SYS_write
+    call    kernel
+    
+    ; 1c clean up the stack
+    add     esp, 12             ; 3 args * 4 bytes/arg = 12 bytes
+
+    ; 2 exit the program
+
+    ; 2a prepare the argument for the sys call to exit
+    push    dword 0             ; exit status returned to the operating system
+
+    ; 2b make the call to sys call to exit
+    mov     eax, SYS_exit
+    call    kernel
+    ; 2c no need to clean up the stack because no code here would executed: already exited
+
+
+; static data segment
+section .data
+
+message   db 'Hello, World!', 0x0a
+length  equ $-hello
 
