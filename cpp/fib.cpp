63963aca513d4fc962142254b06d40720816d972
gitmo <seb0rz+github@gmail.com>
2011-10-10 13:03:02 +0200

    Iterative fib impl

--- cpp/fib.cpp
+++ cpp/fib.cpp
@@ -5,17 +5,24 @@
  * fib(i+2) = fib(i+1) + fib(i), i = {0, 1, 2}
  */
 
-int fib(int i){
-    if (i == 0 || i == 1)
-        return 1;
+int fib(int n)
+{
+    int n_0 = 1, n_1 = 1;
+    for (int i = 3; i <= n; i++) {
+        int n_2 = n_0 + n_1;
+        n_0 = n_1;
+        n_1 = n_2;
+    }
+    return n_1;
 }
-
 int main()
 {
     int x;
 
-    printf("Integer X: ");
-    scanf("%i", &x);
+    do {
+      printf("Integer X (>=0): ");
+      scanf("%i", &x);
+    } while (x < 0);
 
     printf("fib(%d) = %d\n", x, fib(x));
 
